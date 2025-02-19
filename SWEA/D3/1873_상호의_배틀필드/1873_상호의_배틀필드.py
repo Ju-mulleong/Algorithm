2647b24b1151dee43fa10eb5925096814f1695ca
unknown <wnwogus1231@naver.com>
2025-02-20 07:20:23 +0900

    solving

--- SWEA/D3/1873_상호의_배틀필드/1873_상호의_배틀필드.py
+++ SWEA/D3/1873_상호의_배틀필드/1873_상호의_배틀필드.py
@@ -17,7 +17,14 @@ for test_case in range(1, 1+T):
     # 높이 H, 너비 W
 
     battle_field = [list(input()) for _ in range(H)]
-    print(battle_field)
+    # print(battle_field)
+
+    N = int(input())
+    # 사용자가 넣을 입력의 개수 N
+
+    input_command = input()
+    # 입력하는 길이가 N인 문자열 input_command
+
     '''
     전차 위치에서 델타응용으로?
     '''
@@ -25,8 +32,20 @@ for test_case in range(1, 1+T):
     # 전차 위치 찾기
     tank_position = find_tank()
 
-    # 전차 위치에서 처음에 주어진 '^><v' 종류에 따라 명령 실행
-    while
+    # 전차 위치에서 입력값 종류에 따라 명령 실행
+    # 포탄 쏠 때 전차가 바라보는 방향 고려해야함
+
+    # 우 상 좌 하
+    di = [0, -1, 0, 1]
+    dj = [1, 0, -1, 0]
+
+    dw = ['R', 'U', 'L', 'D']
+
+    i, j = tank_position
+    for c in input_command:
+        # 이동
+
+
 
 
 
