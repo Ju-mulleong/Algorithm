ac4c9ab8fdbb0064c47746f3429680c4ced9eaeb
unknown <wnwogus1231@naver.com>
2025-02-21 07:17:42 +0900

    sovling

--- 02_algorithm/03_Queue_2/23734_미로1/23734_미로1.py
+++ 02_algorithm/03_Queue_2/23734_미로1/23734_미로1.py
@@ -9,6 +9,12 @@ DFS 활용
 상하좌우 탐색 후 0으로만 들어가기
 도달 가능 여부를 1 또는 0 으로 표시
 
+재귀로 들어가다가 앞에 벽이 있어서 막혔다면 return, 미로니까 정상인덱스가 아닌건 불가능하다 전부 벽에 둘러쌓임
+
+출구에 도달 못한다는걸 어떻게 판단하나?
+try-except?
+계속 반복해서 출입 위치를 5번 방문하면?
+
 '''
 
 
@@ -19,16 +25,11 @@ def find_start(arr):
                 return ii, jj
 
 
-def dfs(n, k, i, j):
+def dfs(i, j):
     global ans
-    di = [0, -1, 0, 1]
-    dj = [1, 0, -1, 0]
+    global si, sj
 
     # 가지치기
-    if ans == 1:
-        return
-
-
 
     # 기본 파트
 
@@ -36,27 +37,19 @@ def dfs(n, k, i, j):
         ans = 1
         return
 
-
     # 유도 파트
     else:
-        g = 1
         for d in range(4):
-            ni = i + di[d]*g
-            nj = j + dj[d]*g
-            while 0 <= ni <= 15 and 0 <= nj <= 15 and arr[ni][nj] != 1:   # 정상인덱스고 벽이 아닐 때
-                # 같은 방향으로 벽 나올때까지 계속 가보고, 벽 바로 앞의 위치로 이동
-
-                g += 1
-                ni = i + di[d] * g
-                nj = j + dj[d] * g
+                ni = i + di[d]
+                nj = j + dj[d]
+                if arr[ni][nj] != 1 and visited[ni][nj] == 0:   # 미로니까 벽에 둘러쌓임, 1칸씩 델타한다면 전부 정상인덱스 내이다. 통로인지만 확인하면 됨.
+                    visited[ni][nj] = 1
+                    dfs(ni, nj)
+                    # 갔다가 돌아오면 그 갔다왔던 방향 밴
 
 
-
-
-
-                while arr[ni][nj] != 1:
-                    dfs(n, k+1, ni, nj)
         return
+
 
 
 
@@ -68,10 +61,21 @@ for test_case in range(1, 1+T):
     N = 16
     # 16*16의 미로
     arr = [list(map(int, input())) for _ in range(N)]
+    cnt = 0
+    di = [0, -1, 0, 1]
+    dj = [1, 0, -1, 0]
+    visited = [[0]*N for _ in range(N)]
+
     ans = 0
-    i, j = find_start(arr)
-    dfs(N ** 2, 0, i, j)
+    # 시작 위치 구하기
+    si, sj = find_start(arr)
+    dfs(si, sj)
 
     print(f'#{test_case} {ans}')
+
+'''
+어차피 출구가 있다면 다시 '방문'하지 않는다.
+찍고 돌아오는건 '방문'이아니다.
+'''
 
 
