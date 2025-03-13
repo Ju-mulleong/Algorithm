f623e1f82e82bc6ec7cc7ca7d51179ee3eb706e4
unknown <wnwogus1231@naver.com>
2025-03-14 00:42:03 +0900

    solving

--- SWEA/A형_대비/1952_수영장/1952_수영장.py
+++ SWEA/A형_대비/1952_수영장/1952_수영장.py
@@ -6,18 +6,24 @@ sys.stdin = open('input.txt', 'r')
     전부 1일 기준으로 이 이용권으로 며칠동안 이용하면 1일 이용권보다 이득인지 체크?
 3달 요금이 중요한듯
 재귀? 3달요금을 0번, 1번, 2번... 최대 4번 사용할 때, 그때의 최적해
-
+부분집합 느낌
 '''
 
 
-def apply_three_month(t, lst, n):
-    for i in range(12):
+def apply_three_month(t, lst, n, j):
+    if n ==
         # 연속된 3달 선택,
         # 이 3달에서 3달요금이 (1달요금과 하루 요금의 모든 조합)보다 저렴한지 확인
-        lst[i]
+        if lst[j] and t < day_month(lst[j:j+3]):
+            lst[j], lst[j+1], lst[j+2] = 0, 0, 0
+            apply_three_month(t, lst, n+1, j)
+
+    return lst
 
 
 
+
+# 주어진 lst에서 하루요금, 한 달치 요금 조합의 최저가격 구하기
 def day_month(d, m, lst):
     # d = 0
     flag = m // d
@@ -39,15 +45,16 @@ def greedy(d, m, t, y):
 
     # 3달 요금 1번, 2번, 3번, 4번 사기
     for i in range(1, 5):
-        # 3달 요금 적용
-        applied_lst = apply_three_month(t, plan_lst, i)
+        for j in range(12):
+            # 3달 요금 적용한 달 0으로 바꿔서 return
+            applied_lst = apply_three_month(t, plan_lst, i, j)
 
-        # 남은 이용일수 m와 d로 채우기
-        price = day_month(d, m, applied_lst)
+            # 남은 이용일수 m와 d로 채우기
+            price = day_month(d, m, applied_lst)
 
-        # 3달 요금 * i + day_month 리턴값
-        # 기존 최솟값과 비교해서 업데이트
-        min_v = min(min_v, t * i + price)
+            # 3달 요금 * i + day_month 리턴값
+            # 기존 최솟값과 비교해서 업데이트
+            min_v = min(min_v, t * i + price)
 
 
 
