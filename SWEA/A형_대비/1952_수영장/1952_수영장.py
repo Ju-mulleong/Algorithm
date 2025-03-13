f19008da221b3815adfc0b1b1f39d034e4e4687d
unknown <wnwogus1231@naver.com>
2025-03-14 07:20:48 +0900

    solving

--- SWEA/A형_대비/1952_수영장/1952_수영장.py
+++ SWEA/A형_대비/1952_수영장/1952_수영장.py
@@ -10,17 +10,18 @@ sys.stdin = open('input.txt', 'r')
 '''
 
 
-def apply_three_month(t, lst, n, j):
-    if n ==
-        # 연속된 3달 선택,
-        # 이 3달에서 3달요금이 (1달요금과 하루 요금의 모든 조합)보다 저렴한지 확인
-        if lst[j] and t < day_month(lst[j:j+3]):
+def apply_three_month(d, m, t, lst, n, cnt):
+    if n == cnt:
+        # 남은 이용일수 m와 d로 채우기
+        return day_month(d, m, lst)
+
+    for j in range(12):
+        if lst[j]:
+            temp_lst = lst  # 원복용 temp_lst, 1차원리스트니까 그냥 할당해도된다.
+
             lst[j], lst[j+1], lst[j+2] = 0, 0, 0
-            apply_three_month(t, lst, n+1, j)
-
-    return lst
-
-
+            apply_three_month(d, m, t, lst, n, cnt+1)
+            lst = temp_lst
 
 
 # 주어진 lst에서 하루요금, 한 달치 요금 조합의 최저가격 구하기
@@ -29,42 +30,26 @@ def day_month(d, m, lst):
     flag = m // d
     temp_sum = 0
     for i in range(12):
-        if plan_lst[i] > flag:  # 한 달 기준 한달요금이 하루요금보다 이득이면
+        if lst[i] > flag:  # 한 달 기준 한달요금이 하루요금보다 이득이면
             temp_sum += m
-            plan_lst[i] = 0
+            lst[i] = 0
         else:  # 손해면
             temp_sum += d
 
     return temp_sum
 
 
-def greedy(d, m, t, y):
-    global plan_lst
-    # 1년짜리가 디폴트 비용
-    min_v = y
+def greedy(d, m, t):
+    global plan_lst, min_v
 
     # 3달 요금 1번, 2번, 3번, 4번 사기
-    for i in range(1, 5):
-        for j in range(12):
-            # 3달 요금 적용한 달 0으로 바꿔서 return
-            applied_lst = apply_three_month(t, plan_lst, i, j)
+    for n in range(1, 5):
+        # 3달 요금 적용한 달 0으로 바꿔서 return
+        price = apply_three_month(d, m, t, plan_lst, n, 0)
 
-            # 남은 이용일수 m와 d로 채우기
-            price = day_month(d, m, applied_lst)
-
-            # 3달 요금 * i + day_month 리턴값
-            # 기존 최솟값과 비교해서 업데이트
-            min_v = min(min_v, t * i + price)
-
-
-
-
-
-
-
-
-
-
+        # 3달 요금 * i + day_month 리턴값
+        # 기존 최솟값과 비교해서 업데이트
+        min_v = min(min_v, t * n + price)
 
 
 T = int(input())
@@ -77,5 +62,10 @@ for test_case in range(1, 1+T):
     # 이용계획
     plan_lst = list(map(int, input().split()))
     print(plan_lst)
-
     print('-----------------------------------------')
+
+    # 1년짜리가 디폴트 비용
+    min_v = year
+    greedy(day, month, three_month)
+
+    print(f'#{test_case} {min_v}')
