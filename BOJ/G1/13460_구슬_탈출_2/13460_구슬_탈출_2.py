f2017ff51cfecfc9b79cff5b83a53f7733bde8c2
wnwogus <wnwogus1231@naver.com>
2025-03-25 00:25:21 +0900

    solving

---
+++ BOJ/G1/13460_구슬_탈출_2/13460_구슬_탈출_2.py
@@ -0,0 +1,23 @@
+import sys, pprint
+sys.stdin = open('input.txt', 'r')
+
+'''
+빨간공과 파란공이 동시에 움직인다.
+끝행/열은 막혀있다.
+같은 칸에 두 구슬은 들어갈 수 없다.
+빨간 구슬을 빼내는 최소 시도수 출력 
+10번 이하로 움직여서 빨간 구슬을 구멍을 통해 빼낼수 없으면 -1 출력
+최단거리니까 BFS?
+'''
+
+
+def bfs():
+
+
+
+N, M = map(int, input().split())
+# N*M 보드
+
+arr = [list(input()) for _ in range(N)]
+pprint.pprint(arr)
+
