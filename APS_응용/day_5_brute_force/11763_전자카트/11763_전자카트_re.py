a8c3dbed0823bab75ec04a059b74f27cb1a8ef6f
wnwogus <wnwogus1231@naver.com>
2025-03-31 07:53:41 +0900

    solving

--- APS_응용/day_5_brute_force/11763_전자카트/11763_전자카트_re.py
+++ APS_응용/day_5_brute_force/11763_전자카트/11763_전자카트_re.py
@@ -9,10 +9,37 @@ sys.stdin = open('input.txt', 'r')
 재귀 사용 
 '''
 
+# 2~N으로 만들수 있는 순열중 한 가지 주어짐
+def cal(fac):
+    # 일단 양 끝값 먼저 더하기
+    sum_v = arr[1][fac[0]] + arr[fac[-1]][1]
 
-def make_pactorial():
+    # 중간에 값들 더하기
+    # 주어진 순열 2개씩
+    for i in range(len(fac)-1):
+        sum_v += arr[fac[i]][fac[i+1]]
 
-    pass
+    return sum_v
+
+
+# N이 주어지면, 2~N으로 순열 만들기
+def make_factorial(N, fac):
+    global min_v, visited
+    # print(fac)
+
+    if len(fac) == N-1:
+        # 배터리 소비량 계산 함수
+        sum_v = cal(fac)
+        min_v = min(min_v, sum_v)
+        return
+
+    for i in range(2, N+1):
+        if visited[i]:
+            continue
+        visited[i] = 1
+        make_factorial(N, fac + [i])
+        visited[i] = 0
+
 
 T = int(input())
 
@@ -25,5 +52,8 @@ for test_case in range(1, 1+T):
     for i in range(1, N+1):
         arr[i].extend(list(map(int, input().split())))
 
+    visited = [0] * (1 + N)
+    min_v = float('inf')
+    make_factorial(N, [])
 
-
+    print(f'#{test_case} {min_v}')
