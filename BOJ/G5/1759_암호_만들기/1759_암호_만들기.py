b1c9e858480ba4fbbc225d4b4501fd4039c6153b
Ju jaehyeon <wnwogus1231@naver.com>
2025-04-22 09:28:44 +0900

    solved

--- BOJ/G5/1759_암호_만들기/1759_암호_만들기.py
+++ BOJ/G5/1759_암호_만들기/1759_암호_만들기.py
@@ -1,14 +1,12 @@
-import sys
-sys.stdin = open('input.txt', 'r')
-
-'''
-
-'''
+# import sys
+# sys.stdin = open('input.txt', 'r')
 
 
 # dfs(모음의 개수, 자음의 개수)
 def dfs(i, j, idx, password):
     global visited
+
+    # print(password)
 
     # 종료조건
     if len(password) == L:
@@ -18,23 +16,21 @@ def dfs(i, j, idx, password):
         print(password)
         return
 
-    # 가지치기
-
     for ii in range(idx, C):
         # 이번 글자가 모음이라면,
         if password_lst[ii] in lst_c:
-            if visited:
+            if visited[ii]:
                 continue
             visited[ii] = 1
-            dfs(i+1, j, idx+1, password+f'{password_lst[ii]}')
+            dfs(i+1, j, ii+1, password+f'{password_lst[ii]}')
             visited[ii] = 0
 
         # 이번 글자가 자음이라면
         else:
-            if visited:
+            if visited[ii]:
                 continue
             visited[ii] = 1
-            dfs(i, j+1, idx+1, password+f'{password_lst[ii]}')
+            dfs(i, j+1, ii+1, password+f'{password_lst[ii]}')
             visited[ii] = 0
 
         # # dfs 나오면 인덱스만 증가
@@ -60,23 +56,12 @@ for i in range(C):
     else:
         lst_v.append(password_lst[i])
 
-print(password_lst)
-print(lst_c)
-print(lst_v)
+# print(password_lst)
+# print(lst_c)
+# print(lst_v)
 
 len_c = len(lst_c)
 len_v = len(lst_v)
 
-# # 모음 개수 정하기
-# for i in range(1, len_c+1): # 모음의 개수 1개부터 ~ 전체
-#     # 모음 개수가 암호 길이 넘어가면 안 된다.
-#     if i > L:
-#         break
-#
-#     # 자음 개수 정하기
-#     j = L-i
-#     # 자음 개수 2보다 작으면 안 된다. or 전체 주어진 자음개수 넘어가면 안된다.
-#     if j < 2 or j > len_v:
-#         break
 
 dfs(0, 0, 0, '')
